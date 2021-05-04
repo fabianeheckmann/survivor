31175ea233f65b90b6511d5def6cc8d150adf4dc
fabianeheckmann <fabiane.heckmann@bandtec.com.br>
2021-05-04 17:57:19 -0300

    dando inicio a primeira parte do site e adicionando informações

---
+++ site - suvivor/assets/script/scriptindex.js
@@ -0,0 +1,21 @@
+function mostra_dime() {
+
+    dime.style.display="block";
+    inst.style.display="none";
+    item.style.display="none";
+    
+}
+
+function mostra_inst() {
+    dime.style.display="none";
+    inst.style.display="block";
+    item.style.display="none";
+    
+}
+
+function mostra_item() {
+    dime.style.display="none";
+    inst.style.display="none";
+    item.style.display="block";
+    
+}
