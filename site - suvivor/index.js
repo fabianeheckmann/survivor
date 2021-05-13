142ff530edca845657426882222c66b46f972da3
fabianeheckmann <fabiane.heckmann@bandtec.com.br>
2021-05-13 01:17:07 -0300

    att site Earth Song

---
+++ site - suvivor/index.js
@@ -0,0 +1,21 @@
+function mostrar_amaz() {
+
+    amaz.style = 'display: block';
+    pant.style = 'display: none';
+    menos.style = 'display: block'
+}
+
+function mostrar_pant() {
+
+    amaz.style = 'display: none';
+    pant.style = 'display: block';
+    menos.style = 'display: block'
+}
+
+function esconder() {
+    amaz.style = 'display: none';
+    pant.style = 'display: none';
+    menos.style = 'display: none'
+}
+
+
