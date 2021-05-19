e4cb319d17275fdb60b0624b96f0e6bed39fa44f
fabianeheckmann <fabiane.heckmann@bandtec.com.br>
2021-05-19 16:14:26 -0300

    adicionando aba noticias, informações e parte do codigo comentado

---
+++ site - suvivor/assets/script/noticias.js
@@ -0,0 +1,46 @@
+
+function voltar1() {
+    notIn.style = 'display: block';
+    notAm.style = 'display: block';
+    notPt.style = 'display: block';
+    infIn.style = 'display: none';
+}
+
+
+function voltar2() {
+    notIn.style = 'display: block';
+    notAm.style = 'display: block';
+    notPt.style = 'display: block';
+    infAm.style = 'display: none';
+}
+
+
+function voltar3() {
+    notIn.style = 'display: block';
+    notAm.style = 'display: block';
+    notPt.style = 'display: block';
+    infPt.style = 'display: none';
+}
+
+
+
+function indios() {
+    notIn.style = 'display: none';
+    notAm.style = 'display: none';
+    notPt.style = 'display: none';
+    infIn.style = 'display: block';
+}
+
+function amazonia() {
+    notIn.style = 'display: none';
+    notAm.style = 'display: none';
+    notPt.style = 'display: none';
+    infAm.style = 'display: block';
+}
+
+function pantanal() {
+    notIn.style = 'display: none';
+    notAm.style = 'display: none';
+    notPt.style = 'display: none';
+    infPt.style = 'display: block';
+}
