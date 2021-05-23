17cc499bf26db9463b1b07a72a65566ee548f1d4
fabianeheckmann <79323919+fabianeheckmann@users.noreply.github.com>
2021-05-23 19:19:58 -0300

    Add files via upload

--- tabelamsql.sql
+++ tabelamsql.sql
@@ -2,13 +2,28 @@ create database survivor;
 
 use survivor;
 
-create table usuario (
-idUsuario int primary key,
-nome varchar (45),
-email varchar (45),
-senha password varchar (20),
-usuario varchar (20));
-
-create table ong (
-idOng int primary key),
-;
+CREATE TABLE IF NOT EXISTS ong (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45),
+  `cidade` VARCHAR(45),
+  `cep` VARCHAR(10),
+  `estado` VARCHAR(45),
+  PRIMARY KEY (`id`));
+  
+  
+  CREATE TABLE IF NOT EXISTS usuario(
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45),
+  `login` VARCHAR(45),
+  `senha` VARCHAR(45),
+  `cidade` VARCHAR(45),
+  `estado` VARCHAR(45),
+  `cep` VARCHAR(45),
+  PRIMARY KEY (`id`));
+  
+  CREATE TABLE IF NOT EXISTS usuario_ong (
+   fkUsuario int, 
+   foreign key (fkUsuario) references usuario(id),
+   fkOng int,
+   foreign key (fkOng) references ong(id)
+  );
