8455b21de65fafbff9db65045c7b150103baf2aa
fabianeheckmann <fabiane.heckmann@bandtec.com.br>
2021-05-22 21:34:18 -0300

    adiconando tela de login

---
+++ tabelamsql.sql
@@ -0,0 +1,14 @@
+create database survivor;
+
+use survivor;
+
+create table usuario (
+idUsuario int primary key,
+nome varchar (45),
+email varchar (45),
+senha password varchar (20),
+usuario varchar (20));
+
+create table ong (
+idOng int primary key),
+;
