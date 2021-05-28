da36915610e5fc31901be5a1e97b38143adf7985
fabianeheckmann <bertucci.henrique@gmail.com>
2021-05-28 14:14:01 -0300

    pagina povos finalizada

--- API-projeto-site/public/assets/script/povos.js
+++ API-projeto-site/public/assets/script/povos.js
@@ -1,3 +1,57 @@
+
+function fechar() {
+    idPataxo.style = 'display: none';
+    idXavante.style = 'display: none';
+    fechar.style = 'display: none';
+    idCariri.style = 'display: none';
+    idYanomami.style = 'display: none';
+    idCaraja.style = 'display: none'
+    idTupi.style = 'display: none'
+    idPancaruru.style = 'display: none'
+    idTicuna.style = 'display: none'
+    idCainganque.style = 'display: none'
+
+}
+
 function  pataxo() {
-    idPataxo.style = 'display: block'
+    idPataxo.style = 'display: block';
+    fechar.style = 'display: block' ;
+}
+
+function xavante() {
+    idXavante.style = 'display: block';
+    fechar.style = 'display: block';
+}
+
+function cariri() {
+    idCariri.style = 'display: block';
+    fechar.style = 'display: block';
+}
+
+function yanomami() {
+    idYanomami.style = 'display: block';
+    fechar.style = 'display: block';
+}
+
+function tupi() {
+    idTupi.style = 'display: block'
+    fechar.style = 'display: block'
+}
+
+function caraja() {
+    idCaraja.style = 'display: block'
+    fechar.style = 'display: block'
+
+}
+
+function pancaruru() {
+    idPancaruru.style = 'display: block'
+}
+
+function ticuna() {
+    idTicuna.style = 'display: block'
+}
+
+function cainganque() {
+    idCainganque.style = 'display: block'
 }
