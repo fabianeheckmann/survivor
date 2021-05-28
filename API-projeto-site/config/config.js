c076fb935fece76e5f1a790757c807eb066da35c
fabianeheckmann <jennifer.silva@bandtec.com.br>
2021-05-28 16:12:18 -0300

    ultima pág

--- API-projeto-site/config/config.js
+++ API-projeto-site/config/config.js
@@ -26,7 +26,7 @@ module.exports = {
   dev: {
     // altere APENAS username, password e database.
     username: 'root',
-    password: 'lau39039',
+    password: 'bandtec',
     database: 'survivor',
     host: '127.0.0.1',
     dialect: 'mysql',
