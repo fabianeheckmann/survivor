901eee1f83bc6f89b0aa9fa2cb34081273448135
Jennifer Silva <jennifer.silva@bandtec.com.br>
2021-06-02 15:13:51 -0300

    finalizado

--- API-projeto-site/config/config.js
+++ API-projeto-site/config/config.js
@@ -26,7 +26,7 @@ module.exports = {
   dev: {
     // altere APENAS username, password e database.
     username: 'root',
-    password: '',
+    password: 'bandtec',
     database: 'survivor',
     host: '127.0.0.1',
     dialect: 'mysql',
