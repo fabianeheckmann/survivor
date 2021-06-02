786924864d5f4f380a813db0dd82243a3ead07a7
fabianeheckmann <fabiane.heckmann@bandtec.com.br>
2021-06-01 22:22:06 -0300

    páginas finais finalizadas

--- API-projeto-site/config/config.js
+++ API-projeto-site/config/config.js
@@ -26,7 +26,7 @@ module.exports = {
   dev: {
     // altere APENAS username, password e database.
     username: 'root',
-    password: 'bandtec',
+    password: '',
     database: 'survivor',
     host: '127.0.0.1',
     dialect: 'mysql',
